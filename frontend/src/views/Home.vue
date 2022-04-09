3b1c39445f83eff76c7ee928019cbd8f20d81cfb
tathusphol <63070065@kmitl.ac.th>
2022-04-09 17:54:55 +0700

    Merge branch 'main' of https://github.com/tathusphol/Project_webpro

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <nav
       class="flex items-center px-12 py-3 justify-between"
-      style="background: rgb(238, 237, 237);" v-if="loginuser == null"
+      style="background: rgb(238, 237, 237)"
+      v-if="loginuser == null"
     >
       <div class="space-x-6 flex items-center">
         <a href="/" class="text-3xl text-black font-bold">CAR USED</a>
@@ -18,13 +19,16 @@
         </div>
         <div class="flex items-center space-x-2">
           <img width="40px" src="" alt="" />
-          <router-link class="card-footer-item" to="/login">เข้าสู่ระบบ/สมัครสมาชิก</router-link>
+          <router-link class="card-footer-item" to="/login"
+            >เข้าสู่ระบบ/สมัครสมาชิก</router-link
+          >
         </div>
       </div>
     </nav>
     <nav
       class="flex items-center px-12 py-3 justify-between"
-      style="background: rgb(238, 237, 237);" v-else
+      style="background: rgb(238, 237, 237)"
+      v-else
     >
       <div class="space-x-6 flex items-center">
         <a href="/" class="text-3xl text-black font-bold">CAR USED</a>
@@ -39,12 +43,11 @@
           <span>099xxxxxxx</span>
         </div>
         <div class="flex items-center space-x-2">
-            <img width="40px" src="/images/browser/user.png" alt="">
-            <a href="/login">{{loginuser.user_name}}</a>
+          <img width="40px" src="/images/browser/user.png" alt="" />
+          <a href="/login">{{ loginuser.user_name }}</a>
         </div>
       </div>
     </nav>
-    
   </div>
 </template>
 
@@ -55,16 +58,16 @@ export default {
   name: "Home",
   data() {
     return {
-      loginuser : [],
+      loginuser: [],
     };
   },
-  mounted(){
-    this.getdata()
+  mounted() {
+    this.getdata();
   },
-  methods :{
-    getdata(){
-      this.loginuser = JSON.parse(localStorage.getItem('user'))
-    }
-  }
+  methods: {
+    getdata() {
+      this.loginuser = JSON.parse(localStorage.getItem("user"));
+    },
+  },
 };
 </script>
