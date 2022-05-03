06fa346d1503211529a184a8cbbf5d8fb7fc4cbb
63070051 <73683531+63070051@users.noreply.github.com>
2022-05-03 20:27:55 +0700

    update histoeyseller.vue

--- frontend/src/views/seller/historyseller.vue
+++ frontend/src/views/seller/historyseller.vue
@@ -1,6 +1,193 @@
 <template>
   <div id="app">
     <Navbar />
+    <div class="max-w-6xl mx-auto py-4" style="min-height: 500px;">
+      <p class="text-4xl mt-8">History Seller</p>
+      <div class="w-24 h-1 bg-blue-600 mt-2 rounded-lg"></div>
+      <div class="grid grid-cols-3 gap-8">
+        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+            <button class="bg-sky-800 w-24 rounded rounded-lg  text-white" >
+              edit
+            </button>
+                </div>
+              </div>
+            </div>
+        <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+                    <button class="bg-sky-800 w-24 rounded rounded-lg  text-white" >
+                      edit
+                    </button>
+                </div>
+              </div>
+            </div>
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+           <button class="bg-sky-800 w-24 rounded rounded-lg text-white  ">
+             edit
+           </button>
+                </div>
+              </div>
+            </div>
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+                  <button class="bg-sky-800 w-24 rounded rounded-lg  text-white" >
+                    edit
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+                                <button class="bg-sky-800 w-24 rounded rounded-lg  text-white" >
+                                  edit
+                                </button>
+                </div>
+              </div>
+            </div>
+            <div class="rounded-lg shadow-lg bg-white max-w-sm">
+              <div data-mdb-ripple="true" data-mdb-ripple-color="light">
+                <img
+                  class="rounded-t-lg"
+                  src="https://cdn.discordapp.com/attachments/958256273592307722/964466396505473034/unknown.png"
+                  alt=""
+                />
+              </div>
+
+              <div class="p-6 space-y-3">
+                <h5 class="text-gray-900 text-xl font-bold">
+                  222
+                </h5>
+                <p class="text-gray-700 text-base">
+                  222
+                </p>
+                <p class="pb-2 border-b border-gray-300">
+                  1111
+                </p>
+                <div class="flex justify-between">
+                  <p class="text-orange-500 font-bold text-xl float-right">
+                    2222
+                  </p>
+                                  <button class="bg-sky-800 w-24 rounded rounded-lg  text-white" >
+                                    edit
+                                  </button>
+                </div>
+              </div>
+            </div>
+            
+        
+         
+           
+             
+               
+        
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
